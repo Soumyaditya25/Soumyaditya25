12db89d9a2fbdbe7a0dddcf5991650c1c5c4b1c9
Soumyaditya Batabyal <soumyaditya2021@gmail.com>
2024-12-11 01:23:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const axios = require('axios');
 const fs = require('fs');
+const path = require('path');
 
 axios.get('https://api.quotable.io/random')
-  .then(({ data }) => {
-    const quote = `"${data.content}" - ${data.author}`;
-    const content = fs.readFileSync('README.md', 'utf8');
-    const updatedContent = content.replace(/## ❤️ Today's Inspiration(.|\n)*?\n## 🔥/, `## ❤️ Today's Inspiration\n\n"${quote}"\n\n## 🔥`);
-    fs.writeFileSync('README.md', updatedContent);
+  .then(response => {
+    const quote = response.data.content;
+    const author = response.data.author;
+    const date = new Date().toDateString();
+    const readmePath = path.join(process.env.GITHUB_WORKSPACE, 'README.md');
+    const existingContent = fs.readFileSync(readmePath, 'utf8');
+    const newContent = `\n\n## Today's Inspiration (${date})\n\n"${quote}" - ${author}\n`;
+    fs.appendFileSync(readmePath, newContent);
   })
-  .catch(console.error);
+  .catch(error => console.error(error));
